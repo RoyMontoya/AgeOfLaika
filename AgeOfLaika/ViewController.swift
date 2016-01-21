724969abb43794087d3004487ed9b53768037eda
RoyMontoya <roy_a18@hotmail.com>
2016-01-20 21:58:43 -0700

    BitFountain AgeOfLaika excercise

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var dogYearsLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var dogYearsTextField: UITextField!
+    
+    
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ConvertDogYearsButtonPressed(sender: UIButton) {
+        let inputNumber = Int(dogYearsTextField.text!)
+        dogYearsLabel.hidden = false
+        let dogYearsConstant = 7
+        dogYearsLabel.text = "\(inputNumber! * dogYearsConstant)"
+        dogYearsTextField.text = ""
+        dogYearsTextField.resignFirstResponder()
+    }
 
+    @IBAction func ConvertRealDogButtonPressed(sender: UIButton) {
+        let beforeTwoConstant:Double = 10.5
+        let afterTwoConstant:Double = 4
+        let inputNumber = dogYearsTextField.text
+        let doubleInputNumber = Double((inputNumber! as NSString).doubleValue)
+        var realDogYears: Double
+        
+        
+        if doubleInputNumber > 2{
+            realDogYears = ((beforeTwoConstant * 2) + ((doubleInputNumber - 2) * afterTwoConstant))
+        }else{
+            realDogYears = beforeTwoConstant * doubleInputNumber
+        }
+        dogYearsLabel.text = "\(realDogYears)"
+        dogYearsLabel.hidden = false
+    }
 }
 
